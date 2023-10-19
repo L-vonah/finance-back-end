230892b2e0573249a40f072865aff41de1dbc641
Lucas Von Ah <lucas_vonah@hotmail.com>
2023-10-18 21:54:03 -0300

    Add base entity migration on expense

---
+++ Finance/Migrations/20231019005342_ExpenseBaseEntityMigration.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Finance.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExpenseBaseEntityMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Expenses",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Expenses",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Expenses",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Expenses");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "Expenses");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "Expenses");
+        }
+    }
+}
--- Infrastructure/Data/Entity.cs
+++ Infrastructure/Data/Entity.cs
@@ -8,7 +8,7 @@
     public abstract class BaseEntity : SoftableDeleted
     {
         public int Id { get; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
     }
 }
