b720f77b7de083bb1d135f9bd4100cb6368d4fb8
Lucas Von Ah <lucas_vonah@hotmail.com>
2023-10-19 20:50:57 -0300

    Add pagination

---
+++ Infrastructure/Data/Pagination.cs
@@ -0,0 +1,52 @@
+﻿namespace Infrastructure.Data
+{
+    public enum SortOrder { Ascending, Descending }
+
+    public class PageFilter
+    {
+        /// <summary>Page number</summary>
+        public int Page { get; set; }
+
+        /// <summary>Number of items per page</summary>
+        public int PageSize { get; set; } = 10;
+
+        /// <summary>Sort by property name</summary>
+        public string? SortBy { get; set; }
+
+        /// <summary>Sort order</summary>
+        public SortOrder? SortOrder { get; set; }
+
+        public void Validate(Type classType)
+        {
+            if (Page < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(Page), "Page number must be greater than 0");
+            }
+            if (PageSize < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(PageSize), "Page size must be greater than 0");
+            }
+            if (SortBy != null)
+            {
+                var property = classType.GetProperty(SortBy);
+                if (property == null)
+                {
+                    throw new ArgumentException($"Property {SortBy} does not exist in {classType.Name}");
+                }
+            }
+        }
+    }
+
+    public class SearchableFilter : PageFilter
+    {
+        public string Search { get; set; } = string.Empty;
+    }
+
+    public class PaginatedList<T>
+    {
+        public List<T> Items { get; set; } = new List<T>();
+        public int TotalItems { get; set; }
+        public int TotalPages { get; set; }
+        public int Page { get; set; }
+    }
+}
---
+++ Infrastructure/Extensions/PaginationExtensions.cs
@@ -0,0 +1,46 @@
+﻿using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Extensions
+{
+    public static class PaginationExtensions
+    {
+        public static async Task<PaginatedList<T>> ToPaginatedListAsync<T>(this IQueryable<T> source, int pageIndex, int pageSize)
+        {
+            var totalItems = await source.CountAsync();
+            var items = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
+            var totalPages = (int)Math.Ceiling(totalItems / (double)pageSize);
+
+            return new PaginatedList<T>
+            {
+                Items = items,
+                TotalItems = totalItems,
+                TotalPages = totalPages,
+                Page = pageIndex
+            };
+        }
+
+        public static async Task<PaginatedList<T>> ToPaginatedListAsync<T>(this IQueryable<T> source, PageFilter filter)
+        {
+            filter.Validate(typeof(T));
+            if (!string.IsNullOrEmpty(filter.SortBy))
+            {
+                var property = typeof(T).GetProperty(filter.SortBy);
+                if (property != null)
+                {
+                    source = filter.SortOrder == SortOrder.Descending
+                        ? source.OrderByDescending(x => property.GetValue(x, null))
+                        : source.OrderBy(x => property.GetValue(x, null));
+                }
+            }
+            else
+            {
+                source = filter.SortOrder == SortOrder.Descending
+                    ? source.OrderByDescending(x => x)
+                    : source.OrderBy(x => x);
+            }
+
+            return await source.ToPaginatedListAsync(filter.Page, filter.PageSize);
+        }
+    }
+}
